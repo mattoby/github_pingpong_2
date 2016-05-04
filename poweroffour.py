684f654ea39fceb95048a2912cb9e3ce8afc3e77
mattoby <mattoby@gmail.com>
2016-05-04 14:02:45 -0400

    solving fourth root problem

---
+++ poweroffour.py
@@ -0,0 +1,32 @@
+'''
+Problem:
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+Example:
+Given num = 16, return true. Given num = 5, return false.
+Follow up: Could you solve it without loops/recursion?
+'''
+
+#class Solution(object):
+#def isPowerOfFour(self, num):
+
+def isSquare(num):
+    '''
+    Determines if a number 'num' is a square.
+    '''
+
+
+def isPowerOfFour(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+
+    issquare, root = isSquare(num)
+    if issquare:
+        ispoweroffour, fourthroot = isSquare(root)
+
+    return ispoweroffour
+
+
+
+
